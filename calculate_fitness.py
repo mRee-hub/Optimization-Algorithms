67552a699d0c66996defc75b82454cae5411bb95
Emre Soysal <93617238+mRee-hub@users.noreply.github.com>
2024-05-16 20:13:30 +0300

    Add files via upload
    Bu kodda (-5,5) aralığında random verilen 5 adet değerin , karelerinin toplamının sonucu yazdırılmaktadır.

---
+++ calculate_fitness.py
@@ -0,0 +1,18 @@
+import random
+
+def Calculate(data):
+    result = 0
+    for value in data :
+        result += value * value
+    return result
+
+def main():
+    X = [0] * 5
+    for i in range(len(X)):
+        X[i] = 5 - random.randint(0, 10)
+        print(f"X[x{i+1}] = {X[i]}")
+    obj_value = Calculate(X)
+    print("Amaç fonksiyon değeri:",obj_value)
+    
+if __name__ == "__main__":
+    main()
